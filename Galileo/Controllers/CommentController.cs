69d048ebb77f0886749f25de9059d9e31e0eef9a
Scott Brady <scott@prolio.com>
2015-09-16 21:44:37 -0500

    Added *hopefully* all the controllers we will need

---
+++ Galileo/Controllers/CommentController.cs
@@ -0,0 +1,39 @@
+﻿using Galileo.Models;
+using System.Web.Mvc;
+
+namespace Galileo.Controllers
+{
+    public class CommentController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Show all comments stored for the particular user
+            // Should show all comments received and given
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create()
+        {
+            // This will add the comment to the DB and redirect back to the comment page
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit()
+        {
+            // This will edit the comment in the DB and redirect back to the comment page
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete()
+        {
+            // This will delete the comment from the DB and redirect back to the comment page
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Galileo/Controllers/TeamController.cs
@@ -0,0 +1,45 @@
+﻿using Galileo.Models;
+using System.Web.Mvc;
+
+namespace Galileo.Controllers
+{
+    public class TeamController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Show all courses that teams can be created for
+            return View();
+        }
+
+        public ActionResult Course(int courseId)
+        {
+            // Show any teams in the course or offer to create some from the projects in the course
+            // Show all users in the course so they can be assigned to the teams
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create()
+        {
+            // This will add the course to the DB and redirect back to the course page
+            return RedirectToAction("Course");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit()
+        {
+            // This will edit add or remove students from the teams that have been created
+            return RedirectToAction("Course");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete()
+        {
+            // This will delete the team from the course
+            return RedirectToAction("Course");
+        }
+    }
+}
