d816711ea0135f74fc17a13ff26be3daf80bca85
Scott Brady <scott@prolio.com>
2015-09-17 19:26:05 -0500

    Refactored Report Controller

--- Galileo/Controllers/ReportController.cs
+++ Galileo/Controllers/ReportController.cs
@@ -10,49 +10,88 @@ namespace Galileo.Controllers
 {
     public class ReportController : Controller
     {
+        /// <summary>
+        /// Checks if the logged in user is a teacher, project manager, team leader, or team member and redirects accordingly
+        /// Teachers get sent to the page that lists every course they have access to
+        /// Project Managers get sent to a page that shows what projects they are managing
+        /// Team Leaders get sent to their team page
+        /// Team Members get sent to their individual page
+        /// </summary>
         public ActionResult Index()
+        {
+            User user = GlobalVariables.CurrentUser;
+
+            if (user.user_is_teacher)
+                return RedirectToAction("Courses");
+            else
+                return RedirectToAction("Individual", new { userId = user.user_id });
+        }
+
+        /// <summary>
+        /// Lists all of the courses that a teacher has access to
+        /// Gives a summary of the hours spent in each course
+        /// </summary>
+        public ActionResult Courses()
         {
             TimeMachineRepository db = new TimeMachineRepository();
             User user = GlobalVariables.CurrentUser;
-            // Check if this is a teacher, pm, team leader, or team member
-            // Teachers gets this page with all the courses listed with a summary of hours for each course
-            // PMs get redirected to courses page that only shows them the projects they are over
-            // Team Leaders get redirected to their team page
-            // Team members get redirected to their individual page
             List<Course> courses = db.GetCourses(user.user_id);
             return View(courses);
         }
 
-        public ActionResult Course(int courseId)
+        /// <summary>
+        /// Lists all of the projects and teams inside a course
+        /// Gives a summary of the hours spent in each project/team
+        /// </summary>
+        /// <param name="courseId">The ID of the course that the projects are in</param>
+        public ActionResult Projects(int courseId)
         {
             TimeMachineRepository db = new TimeMachineRepository();
-            // This view will show all of the projects in the course with a summary of hours for each project
             List<Project> projects = db.GetProjects(courseId);
-
             return View(projects);
         }
 
+        /// <summary>
+        /// Lists all of the students that have logged time for a particular course
+        /// Gives a summary of all of the hours the students have logged for that course
+        /// </summary>
+        /// <param name="courseId">The ID of the course to get the individuals for</param>
+        /// <returns></returns>
+        public ActionResult Course(int courseId)
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Lists all of the students that have logged time for a particular project
+        /// Gives a summary of all of the hours the students have logged for that project
+        /// </summary>
+        /// <param name="projectId">The ID of the project to get the individuals for</param>
+        /// <returns></returns>
         public ActionResult Project(int projectId)
         {
-            // This view will show all the teams (or if no teams, individuals) with a summary of hours for each one
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
+        /// <summary>
+        /// Lists all of the students that are in a particular team
+        /// Gives a summary of all of the hours each team member has logged for that team
+        /// </summary>
+        /// <param name="teamId">The ID of the team to get the members for</param>
+        /// <returns></returns>
         public ActionResult Team(int teamId)
         {
-            // This view will show all of the team members with a summary of hours for each one
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
+        /// <summary>
+        /// Gives a detailed view of the individual
+        /// Includes a summary of their hours as well as each time entry
+        /// </summary>
+        /// <param name="userId">The ID of the user to get the information for</param>
+        /// <returns></returns>
         public ActionResult Individual(string userId)
         {
-            // This view will show all of the info for an individual team member
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
