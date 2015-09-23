949673182b018a04f052533e169c7fa725e84f32
unknown <121508@AC212-17.studentnet.int>
2015-09-23 16:09:34 -0500

    Starting Comments

---
+++ Galileo/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Galileo.Models
+{
+    public class Comment
+    {
+        public string recipient_id { get; set; }
+        public string commenter_id { get; set; }
+        public DateTime created_at { get; set; }
+        public string comment { get; set; }
+    }
+}
---
+++ Galileo/ViewModels/Comments.cs
@@ -0,0 +1,15 @@
+﻿using Galileo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Galileo.ViewModels
+{
+    public class Comments
+    {
+        public List<Comment> comments { get; set; }
+        public string recipients { get; set; }
+        public string comment { get; set; }
+    }
+}
