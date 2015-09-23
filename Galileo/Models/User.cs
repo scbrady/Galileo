900a7e2c29422d1751eb0720b31883e2100957dd
unknown <121508@AC212-17.studentnet.int>
2015-09-23 16:17:40 -0500

    Implemented DB changes

--- Galileo/Models/User.cs
+++ Galileo/Models/User.cs
@@ -9,6 +9,8 @@
         public bool user_is_student { get; set; }
         public bool user_is_teacher { get; set; }
         public bool user_is_manager { get; set; }
+        public bool user_is_team_leader { get; set; }
+        public bool user_is_project_manager { get; set; }
         public int user_total_hours { get; set; }
     }
 }
