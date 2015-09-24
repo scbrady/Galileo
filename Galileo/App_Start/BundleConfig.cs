b4af445b2fe0b08170311fc9e078a65713a8ece3
unknown <121508@AC212-16.studentnet.int>
2015-09-24 16:56:21 -0500

    Selectize

--- Galileo/App_Start/BundleConfig.cs
+++ Galileo/App_Start/BundleConfig.cs
@@ -17,6 +17,9 @@ namespace Galileo
             bundles.Add(new ScriptBundle("~/bundles/jquerysort").Include(
                         "~/Scripts/jquery.sortable*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/selectize").Include(
+                        "~/Scripts/selectize*"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
